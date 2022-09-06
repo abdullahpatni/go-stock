19f9a2d732e9a61852351a82fca483bf7a711731
Abdullah Abdul Gaffar Patni <abdullah@Abdullahs-MacBook-Air.local>
2022-09-06 20:36:27 +0530

    first commit

---
+++ api/stock.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+const url = "https://api.worldtradingdata.com/api/v1/stock"
+const token = "hCJvoIqp6cS1AWSC62m0eKyLXmDDZDBZplcYjX4exLNEzCLYToB8eDxF5OqW"
+
+func Index(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	_, _ = fmt.Fprintf(w, "Stocks service is running....")
+}
+
+func GetStocks(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	symbol := vars["symbol"]
+
+	res, _ := http.Get(fmt.Sprintf("%s?symbol=%s&api_token=%s", url, symbol, token))
+	bytes, _ := ioutil.ReadAll(res.Body)
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Add("Content-Type", "application/json; charset-utf-8")
+	_, _ = w.Write(bytes)
+}
